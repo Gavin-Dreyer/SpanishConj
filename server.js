0f279e209a0b9d196bc520daf7d071c2321e90f2
Gavin Dreyer <gavindreyer1@gmail.com>
2019-11-06 20:38:53 -0800

    returning db with a get request

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express');
 
+const db = require('./data/db-config.js')
+
 const server = express();
 
 server.use(express.json());
 
+function find() {
+    return db.select('*').from('verbs');
+	// return db('verbs');
+}
+
+
 server.get('/', (req, res) => {
-    res.send('<h2>Hello</h2>')
-})
+	find()
+		.then(verbs => {
+			res.status(200).json(verbs);
+		})
+		.catch(err => {
+			res.status(500).json({ message: 'Failed to get verbs' });
+		});
+});
 
 module.exports = server;
